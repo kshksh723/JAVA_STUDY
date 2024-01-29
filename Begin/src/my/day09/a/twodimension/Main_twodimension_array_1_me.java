cc0bd51bd82c96acc4ee416ca701f2ef5ab066ca
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-01-29 12:48:01 +0900

    z

--- Begin/src/my/day09/a/twodimension/Main_twodimension_array_1_me.java
+++ Begin/src/my/day09/a/twodimension/Main_twodimension_array_1_me.java
@@ -1,48 +1,163 @@
+/*
+ * package my.day09.a.twodimension;
+ * 
+ * public class Main_twodimension_array_1_me {
+ * 
+ * public static void main(String[] args) { // ==== 1차원 배열 ==== // int[]
+ * subject_arr = new int[5];
+ * 
+ * ----------------------- 값 ==> | 0 | 0 | 0 | 0 | 0 | -----------------------
+ * index ==> 0 1 2 3 4
+ * 
+ * 
+ * subject_arr[0] = 100; subject_arr[1] = 90; subject_arr[2] = 95;
+ * subject_arr[3] = 80; subject_arr[4] = 88;
+ * 
+ * // ==== 2차원 배열 ==== // int[][] point_arr = new int[4][3]; // 4행 3열
+ * 
+ * 값 ==> -------------- | 0 | 0 | 0 | -------------- | 0 | 0 | 0 |
+ * -------------- | 0 | 0 | 0 | -------------- | 0 | 0 | 0 | --------------
+ * 
+ * index ==> //4행 3열, 4=세로 3=가로 ----------------------------- | [0][0] | [0][1]
+ * | [0][2] | ----------------------------- | [1][0] | [1][1] | [1][2] |
+ * ----------------------------- | [2][0] | [2][1] | [2][2] |
+ * ----------------------------- | [3][0] | [3][1] | [3][2] |
+ * 
+ * 
+ * point_arr[0][0] = 10; point_arr[0][1] = 20; point_arr[0][2] = 30;
+ * 
+ * point_arr[1][0] = 40; point_arr[1][1] = 50; point_arr[1][2] = 60;
+ * 
+ * point_arr[2][0] = 70; point_arr[2][1] = 80; point_arr[2][2] = 90;
+ * 
+ * point_arr[3][0] = 100; point_arr[3][1] = 100; point_arr[3][2] = 100;
+ * 
+ * System.out.println("point_arr.length=" + point_arr.length); // 세로수 (행수) //
+ * point_arr.length : 4 // 2차원배열명.length는 행의 길이가 나온다.
+ * 
+ * System.out.println("point_arr[0].length=" + point_arr[0].length); //
+ * point_arr.length : 3 // 2차원배열명[행의인덱스].length는 그 행에 존재하는 열의 길이(크기)가 나온다.
+ * 
+ * System.out.println("point_arr[1].length=" + point_arr[1].length);
+ * System.out.println("point_arr[2].length=" + point_arr[2].length);
+ * System.out.println("point_arr[3].length=" + point_arr[3].length);
+ * 
+ * System.out.println("===============================--==\n");
+ * 
+ * for(int i = 0; i < point_arr.length; i++) { // 행 for(int j = 0; j <
+ * point_arr[i].length; j++) { String add = (j < point_arr[i].length
+ * -1)?",":"\n"; System.out.printf("%3d%s", point_arr[i][j],add); }
+ * 
+ * }
+ * 
+ * 
+ * 10, 20, 30 40, 50, 60 70, 80, 90 100, 100, 100
+ * 
+ * System.out.println("===== 성적결과 ===== \n"); // 국어 영어 수학 int[][] jumsu_arr = {
+ * {90,80,70}, // 이순신 {80,85,76}, // 엄정화 {80,85,76}, // 홍길동 {85,70,90}, // 공유
+ * {60,80,50} // 아이유 };
+ * 
+ * int[] total_arr = new int[jumsu_arr.length]; // 각 학생별 총점을 저장시켜 두는 곳이다.
+ * 
+ * System.out.println("\n-------------------------------");
+ * System.out.println("국어\t 영어\t 수학\t 총점\t 평균\t 학점\t 등수");
+ * System.out.println("\n-------------------------------");
+ * 
+ * 
+ * 
+ * int[] rank = new int[4]; for(int i = 0; i<jumsu_arr.length; i++) { String
+ * result = ""; int sum = 0;
+ * 
+ * int[] ranknum = new int[4];
+ * 
+ * for(int j = 0; j<jumsu_arr[i].length; j++) { result += jumsu_arr[i][j] +
+ * "\t"; sum += jumsu_arr[i][j]; } rank[i] = sum;
+ * 
+ * for(int j=0; j<jumsu_arr.length; j++) { for(int k=0; k<jumsu_arr.length; k++)
+ * { if(rank[i] < rank[k]) { ranknum[i]++; } } } // end of for
+ * --------------------------
+ * 
+ * 
+ * double avg = Math.round((double)sum/jumsu_arr[i].length * 10)/10.0;
+ * 
+ * char grade = ' ';
+ * 
+ * 
+ * switch ((int)avg/10) { // byte,short,int,char,String만 들어옴 case 10: case 9:
+ * grade = 'A'; break;
+ * 
+ * case 8: grade = 'B'; break;
+ * 
+ * case 7: grade = 'C'; break;
+ * 
+ * case 6: grade = 'D'; break;
+ * 
+ * default: grade = 'F'; break; }// end of switch()---------------
+ * 
+ * 
+ * 
+ * result += sum + "\t" + avg + "\t" + grade + "\t" + ranknum[i];
+ * System.out.println(result);
+ * 
+ * } // end of for--------------------------
+ * 
+ * 
+ * --------------------- 국어 영어 수학 총점 평균 학점 등수 --------------------- 90 80 70 80
+ * 85 76 85 70 90 60 80 50
+ * 
+ * 
+ * 
+ * } // end of main
+ * 
+ * }
+ */
 package my.day09.a.twodimension;
 
 public class Main_twodimension_array_1_me {
 
 	public static void main(String[] args) {
-		// ==== 1차원 배열 ==== //
-		int[] subject_arr = new int[5];
-		/*
-		 	   		-----------------------
-		 값 ==> 		| 0 | 0 | 0 | 0 | 0 |
-		 	   		-----------------------
-		 index ==>	 0    1   2   3   4
-		 	   
-		 * */
+		
+		// === 1차원 배열 === //
+		int[] subject_arr = new int[5];  // 1행 5열
+	/*
+	              ----------------------
+	    값 ==>     | 0 | 0 | 0 | 0 | 0 | 
+	     	      ----------------------
+	    index ==>   0   1   2   3   4  	   
+	*/
 		subject_arr[0] = 100;
 		subject_arr[1] = 90;
 		subject_arr[2] = 95;
 		subject_arr[3] = 80;
 		subject_arr[4] = 88;
 		
-		// ==== 2차원 배열 ==== //
-		int[][] point_arr = new int[4][3]; // 4행 3열
-		/*
-		 * 값 ==> 
-		      		--------------
-		      		| 0 | 0 | 0 |
-					--------------
-				    | 0 | 0 | 0 |
-					--------------
-				    | 0 | 0 | 0 |
-					--------------
-					| 0 | 0 | 0 |
-					--------------
-					
-		   index ==> //4행 3열, 4=세로 3=가로
-		   		  -----------------------------
-		   		  | [0][0] | [0][1] | [0][2] |
-		   		  -----------------------------
-		   		  | [1][0] | [1][1] | [1][2] |
-		   		  -----------------------------
-		   		  | [2][0] | [2][1] | [2][2] |
-				  -----------------------------
-				  | [3][0] | [3][1] | [3][2] |
-		*/
-		
+		// === 2차원 배열 === //
+		int[][] point_arr = new int[4][3];  // 4행 3열
+	/*
+        값 ==> 
+               --------------
+               | 0 | 0 | 0 | 
+	           --------------
+	           | 0 | 0 | 0 |
+	           --------------
+	           | 0 | 0 | 0 |
+	           --------------
+	           | 0 | 0 | 0 |
+	           --------------
+          
+	      
+        index ==>
+               -----------------------------
+               | [0][0] | [0][1] | [0][2] |
+               -----------------------------
+               | [1][0] | [1][1] | [1][2] |
+               -----------------------------
+               | [2][0] | [2][1] | [2][2] |
+               -----------------------------
+               | [3][0] | [3][1] | [3][2] |
+               -----------------------------
+           	   
+*/	
 		point_arr[0][0] = 10;
 		point_arr[0][1] = 20;
 		point_arr[0][2] = 30;
@@ -59,121 +174,209 @@ public class Main_twodimension_array_1_me {
 		point_arr[3][1] = 100;
 		point_arr[3][2] = 100;
 		
-		System.out.println("point_arr.length=" + point_arr.length); // 세로수 (행수)
+		System.out.println("point_arr.length : " + point_arr.length); 
 		// point_arr.length : 4
-		// 2차원배열명.length는 행의 길이가 나온다. 
+		// 2차원배열명.length 은 행의 길이가 나온다. 
 		
-		System.out.println("point_arr[0].length=" + point_arr[0].length);
-		// point_arr.length : 3
-		// 2차원배열명[행의인덱스].length는 그 행에 존재하는 열의 길이(크기)가 나온다.
+		System.out.println("point_arr[0].length : " + point_arr[0].length);  
+		// point_arr[0].length : 3
+		// 2차원배열명[행의인덱스].length 은 그 행에 존재하는 열의 길이(크기)가 나온다.
 		
-		System.out.println("point_arr[1].length=" + point_arr[1].length);
-		System.out.println("point_arr[2].length=" + point_arr[2].length);
-		System.out.println("point_arr[3].length=" + point_arr[3].length);
-		
-		System.out.println("===============================--==\n");
-		
-		for(int i = 0; i < point_arr.length; i++) { // 행
-			for(int j = 0; j < point_arr[i].length; j++) {
-				String add = (j < point_arr[i].length -1)?",":"\n";
-				System.out.printf("%3d%s", point_arr[i][j],add);
-			}
-			
-		}
+		System.out.println("point_arr[1].length : " + point_arr[1].length);  
+		// point_arr[1].length : 3
+		// 2차원배열명[행의인덱스].length 은 그 행에 존재하는 열의 길이(크기)가 나온다.
 
+		System.out.println("point_arr[2].length : " + point_arr[2].length);  
+		// point_arr[2].length : 3
+		// 2차원배열명[행의인덱스].length 은 그 행에 존재하는 열의 길이(크기)가 나온다.
+		
+		System.out.println("point_arr[3].length : " + point_arr[3].length);  
+		// point_arr[3].length : 3
+		// 2차원배열명[행의인덱스].length 은 그 행에 존재하는 열의 길이(크기)가 나온다.
+		
+		System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		
+		for(int i=0; i<point_arr.length; i++) { // 행 
+			
+			for(int j=0; j<point_arr[i].length; j++) { // 열 
+				String add = (j<point_arr[i].length-1)?",":"\n";
+				System.out.printf("%3d%s", point_arr[i][j], add);
+			}// end of for---------------------
+			
+		}// end of for-------------------------
+		
 		/*
 		      10, 20, 30
 		      40, 50, 60
 		      70, 80, 90
-		      100, 100, 100
-		 * */
-		System.out.println("===== 성적결과 ===== \n");
-							// 국어 영어 수학
-		int[][] jumsu_arr = { {90,80,70}, // 이순신
-							  {80,85,76}, // 엄정화
-							  {80,85,76}, // 홍길동
-							  {85,70,90}, // 공유
-							  {60,80,50}  // 아이유
-							};
+		     100,100,100
+		*/
+		
+		System.out.println("\n========= 성적결과 =========\n");
+		
+		                    // 국어 영어 수학
+		int[][] jumsu_arr = { {90,80,70},  // 이순신 
+				              {80,85,76},  // 엄정화
+				              {80,85,76},  // 홍길동
+				              {85,70,90},  // 공유
+				              {60,80,50}   // 아이유
+				            };
 		
 		int[] total_arr = new int[jumsu_arr.length];
-		// 각 학생별 총점을 저장시켜 두는 곳이다.
+		// 각 학생별 총점을 저장시켜두는 곳이다.
 		
-		System.out.println("\n-------------------------------");
-		System.out.println("국어\t 영어\t 수학\t 총점\t 평균\t 학점\t 등수");
-		System.out.println("\n-------------------------------");
+		String[] result_arr = new String[jumsu_arr.length];
+		// 각 학생별로 등수를 제외한 국어,영어,수학,총점,평균,학점까지 성적결과를 저장시켜두는 곳이다.
 		
+		System.out.println("----------------------------------------------------"); 
+		System.out.println("국어\t영어\t수학\t총점\t평균\t학점\t등수");
+		System.out.println("----------------------------------------------------");
 		
-		
-		int[] rank = new int[4];
-		for(int i = 0; i<jumsu_arr.length; i++) {
+		for(int i=0; i<jumsu_arr.length; i++) {
+			
 			String result = "";
 			int sum = 0;
 			
-			int[] ranknum = new int[4];
-			
-			for(int j = 0; j<jumsu_arr[i].length; j++) {
-				result += jumsu_arr[i][j] + "\t"; 
+			for(int j=0; j<jumsu_arr[i].length; j++) {
+				result += jumsu_arr[i][j] + "\t";
 				sum += jumsu_arr[i][j];
-			} 
-			rank[i] = sum;
+			}// end of for---------------------
 			
-			for(int j=0; j<jumsu_arr.length; j++) {		
-				for(int k=0; k<jumsu_arr.length; k++) {
-					if(rank[i] < rank[k]) {
-						ranknum[i]++;
-					}
-				}
-			}
-			// end of for --------------------------
-
-
-			double avg = Math.round((double)sum/jumsu_arr[i].length * 10)/10.0;
+			double avg = Math.round((double)sum/jumsu_arr[i].length * 10)/10.0; 
 			
 			char grade = ' ';
 			
-			
-			switch ((int)avg/10) { // byte,short,int,char,String만 들어옴
-				case 10:
+			switch ((int)avg/10) {
+			    case 10 :
 				case 9:
 					grade = 'A';
 					break;
-					
+	
 				case 8:
 					grade = 'B';
 					break;
 					
 				case 7:
 					grade = 'C';
-					break;
+					break;	
 					
 				case 6:
 					grade = 'D';
-					break;
+					break;	
 					
 				default:
 					grade = 'F';
 					break;
-			}// end of switch()---------------
+			}// end of switch()-----------------
 			
+			result += sum + "\t" + avg + "\t" + grade + "\t";  
+		 //	System.out.println(result);
 			
+			result_arr[i] = result; // 각 학생별로 국어,영어,수학,총점,평균,등급 까지 결과만을 구해서 배열 result_arr 에 저장시켜 둔다.
 			
-			result += sum + "\t" + avg + "\t" + grade + "\t" + ranknum[i];
-			System.out.println(result);
+			total_arr[i] = sum; // 각 학생별 총점을 구해서 배열 total_arr 에 저장시켜 둔다.
+		}// end of for--------------------------
+		
+		
+		// >>> 등수를 구해서 성적결과 출력하기 <<< 
+		for(int i=0; i<total_arr.length; i++) {
 			
-		} // end of for--------------------------
+			int rank = 1;
+			for(int j=0; j<total_arr.length; j++) {
+				
+			   if(i != j &&	total_arr[i] < total_arr[j]) {
+				  // total_arr[i] 이 자신의 총점
+				  // total_arr[j] 가 다른사람의 총점
+				   rank++;
+			   }	   
+			}// end of for-----------------
+			
+			System.out.println(result_arr[i] + rank);
+			
+		}// end of for----------------------
+		
+		
+		System.out.println("====================================================="); 
+		
+		int[] arr_subject_total = new int[jumsu_arr[0].length];
+		// 각 과목별 총점을 저장시켜두는 곳이다.
+		
+		for(int i=0; i<arr_subject_total.length; i++) {
+			
+			for(int j=0; j<jumsu_arr.length; j++) {
+				arr_subject_total[i] += jumsu_arr[j][i];
+			}// end of for----------------------
+		/*	
+			arr_subject_total[0] += jumsu_arr[0][0];
+			                        jumsu_arr[1][0];
+			                        jumsu_arr[2][0];
+			                        jumsu_arr[3][0];
+			                        jumsu_arr[4][0];
+			                        
+			                        
+			arr_subject_total[1] += jumsu_arr[0][1];
+			                        jumsu_arr[1][1];
+			                        jumsu_arr[2][1];
+			                        jumsu_arr[3][1];
+			                        jumsu_arr[4][1];
+			                        
+			                        
+			arr_subject_total[2] += jumsu_arr[0][2];
+			                        jumsu_arr[1][2];
+			                        jumsu_arr[2][2];
+			                        jumsu_arr[3][2];
+			                        jumsu_arr[4][2];			                        
+		*/	
+		}// end of for---------------------------------
+		
+		String str_subject_total = "", str_subject_avg = "";
+		for(int i=0; i<arr_subject_total.length; i++) {
+			str_subject_total += arr_subject_total[i] + "\t";
+			str_subject_avg += Math.round((double)arr_subject_total[i]/jumsu_arr.length * 10)/10.0 + "\t";   
+		}// end of for---------------------
+		
+		System.out.println(str_subject_total);
+		System.out.println(str_subject_avg);
 		
 		/*
-		 ---------------------
-		 국어 영어 수학 총점 평균 학점 등수
-		 ---------------------
-		 90  80  70  
-		 80  85  76
-		 85  70  90
-		 60  80  50
-		 
-		 * */
+			-----------------------------------------------
+			국어	   영어	 수학	      총점	  평균	 학점	    등수
+			-----------------------------------------------
+			90	   80	  70	  240	  80.0	  B 	 4
+			80	   85	  76	  241	  80.3	  B	     2
+			80	   85	  76	  241	  80.3	  B	     2
+			85	   70	  90	  245	  81.7	  B	     1
+			60	   80	  50	  190	  63.3	  D	     5
+		    ===============================================
+		    395	   400    362
+		    79.0   80.0   72.4
+		*/
 		
-	} // end of main
+		
+		System.out.println("\n################################\n");
+		
+		System.out.println(">> 배열 arr_subject_total 에 저장되어진 값을 꺼내오는 첫번째 방법 << ");
+		for(int i=0; i<arr_subject_total.length; i++) {
+			System.out.println(arr_subject_total[i]);
+		}// end of for--------------------
+		/*
+		   395
+           400
+           362 
+		*/
+		
+		System.out.println("\n>> 배열 arr_subject_total 에 저장되어진 값을 꺼내오는 두번째 방법 << ");
+		// 아래의 for 문을 "확장 for문" 또는 "개선된 for문" 이라고 부른다.
+		for(int subject_total : arr_subject_total) {
+			// 반복의 회수는 arr_subject_total 배열의 길이 만큼 한다.
+			// int subject_total = arr_subject_total[0];
+			// int subject_total = arr_subject_total[1];
+			// int subject_total = arr_subject_total[2];
+			System.out.println(subject_total);
+		}// end of for-------------------
+		
+		
+	}// end of main()-------------------------------
 
 }
+
--- Begin/src/my/day10/a/string/Main_String.java
+++ Begin/src/my/day10/a/string/Main_String.java
@@ -3,8 +3,155 @@ package my.day10.a.string;
 public class Main_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// === 1. "문자열".charAt(int index) === 
+	      //        "안녕하세요".charAt(2) ==> '하' 
+	        // index => 01234	
+	      
+	      char ch = "안녕하세요".charAt(2);
+	      System.out.println("ch => " + ch);
+	      // ch => 하
+	      
+	      String str = "안녕하세요";
+	     //    index =>  01234
+	    
+	      String result = "";
+	      for(int i = str.length()-1; i>=0; i--) {
+	    	  result += str.charAt(i);
+	      }
+		
+	      System.out.println(result);
+		
+		// === 2. "문자열".toCharArray() ===
+	      // "안녕하세요".toCharArray()  ==> char 타입의 배열로 만들어준다.
+	      // ------------------------
+	      // |'안'|'녕'|'하'|'세'|'요'|
+	      // ------------------------
+	      //   0    1    2   3    4   <== index
+		
+		char [] ch_arr = str.toCharArray();
+		result = "";
+		
+		for(int i = ch_arr.length-1; i>=0; i--) {
+			result += ch_arr[i];
+		}
+		System.out.println(result);
+		
+		
+		// === 3. "문자열".substring(int 시작인덱스, int 끝인덱스) ===
+	      // "안녕하세요".substring(1, 4); 
+	      // ==> "안녕하세요" 에서 시작인덱스가 1인 글자("녕") 부터
+	      //     "안녕하세요" 에서 끝인덱스 4인 글자("요") 앞(인덱스 3)에까지("세")만 뽑아온다.
+		
+		str = "안녕하세요".substring(1, 4); //5 앞까지
+		System.out.println(str);
+		// 녕하세
+		
+		str = "안녕하세요 오늘도 행복하시고 좋은 하루 되세요~~^^".substring(2,str.length()); //2부터 끝까지는 index번호가 str.length()-> 글자길이 앞까지
+		System.out.println(str);
+		//substring : 일부분만 지운다
+		
+		
+		// === 4. "문자열".substring(int 시작인덱스) ===
+	      // "안녕하세요".substring(2); 
+	      // ==> "안녕하세요" 에서 시작인덱스가 2인 글자("하") 부터 끝까지 뽑아온다.
+		str = "안녕하세요".substring(2);
+		System.out.println(str);
+		// 하세요
+		
+		
+		// === 5. "문자열".indexOf("찾을문자열") ====
+	      //        "문자열" 에서 최초로 나오는 "찾을문자열"의 인덱스(int)를 알려준다.
+		int index = "시작하라 안녕하세요 건강하세요".indexOf("하");
+		// 012
+		System.out.println(index); //2
+		
+		index = "시작하라 안녕하세요 건강하세요".indexOf("하세요");
+		System.out.println(index); //7
+		
+		index = "시작하라 안녕하세요 건강하세요".indexOf("A");
+		System.out.println(index); // -1 찾고자하는 문자열이 없으면 -1이 나온다
+		
+		
+		// ==== [퀴즈] ==== //
+	      System.out.println("\n~~~~~~~~~~~~~~~~~~~\n");
+	      
+	      String[] pathFileNameArr = {"C:/mydocument/resume/나의이력서.hwp",
+	                                "D:/mymusic.mp3",
+	                                "C:/photo/내얼굴.jpg"};
+	      
+	      for(int i= 0; i<pathFileNameArr.length; i++) {
+	    	  System.out.println(pathFileNameArr[i]);
+	      }
+		/* "C:/mydocument/resume/나의이력서.hwp",
+	                                "D:/mymusic.mp3",
+	                                "C:/photo/내얼굴.jpg"
+	                                */
+	      System.out.println("\n 파일명만 뽑아온 결과물 ==");
+	      
+	      for(int i =0; i<pathFileNameArr.length; i++) {
+	    	  char[] chArr = pathFileNameArr[i].toCharArray();
+	    	  
+	    	  String str_reverse = "";
+	    	  for(int j=chArr.length-1; j>=0; j--) {
+	    		  str_reverse += chArr[j];
+	    	  }
+	      }
+	      
+	      
+	      // system.out.println(str_reverse);
+	      /*
+	       pwh.서력의 나/emuser/tnemucodym/:C
+	       3pm.cisumym/:D
+	       gpj.굴얼내/otohp/:C
+	       
+	       */
+	      
+	      /*
+	       *나의이력서.hwp",
+	        mymusic.mp3",
+	      	내얼굴.jpg"
+	       
+	       */
+	      // str_reverse에서 처음부터 최초로 "/"가 나오는 앞까지만 추출하면 된다
+	      // str_reverse.substring
+			/*
+			 * if(idx != -1) { str_reverse = }
+			 * 
+			 * for(int i = 0; i<pathFileNameArr.length; i++) { //행
+			 * 
+			 * for(int j = 0; j<pathFileNameArr.lengh; j++) { //열 for(int j= chArr)
+			 * str_result += chArr[j]; } System.out.printf(str_result);
+			 * 
+			 * }
+			 */
+	      
+	   // === 6. "문자열".lastIndexOf("찾을문자열") ====
+	      //        "문자열" 에서 마지막으로 나오는 "찾을문자열"의 인덱스(int)를 알려준다.
+	      index = "시작하라 안녕하세요 건강하세요".lastIndexOf("하");
+			// 012
+			System.out.println(index); //13
+			
+			index = "시작하라 안녕하세요 건강하세요".lastIndexOf("하세요");//01234567
+			System.out.println(index); //13
+			
+			index = "시작하라 안녕하세요 건강하세요".lastIndexOf("A");
+			System.out.println(index); // -1 찾고자하는 문자열이 없으면 -1이 나온다
+			
+			// ==== [퀴즈] ==== //	
+			  System.out.println("\n 파일명만 뽑아온 결과물 ==");
+			/*  String[] pathFileNameArr =
+			 *  {"C:/mydocument/resume/나의이력서.hwp",
+                      "D:/mymusic.mp3",
+                      "C:/photo/내얼굴.jpg"};
+                      */
+			  for(int i = 0; i<pathFileNameArr.length; i++) {
+				  int idx = pathFileNameArr[i].lastIndexOf("/");
+				  if(idx != -1) {
+					  System.out.println(pathFileNameArr[i].substring(idx+1));
+				  // pathFileNameArr[i].substring(idx+1)은 마지막으로 나오는 "/"의 인덱스 그 다음부터 끝까지 뽑아낸다
+				  }
+				  
+			  }
+	
 	}
-
 }
