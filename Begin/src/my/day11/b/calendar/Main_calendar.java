58e10c0376002a898b509407bac21a9ca94217ba
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-01-30 12:28:02 +0900

    1/30

---
+++ Begin/src/my/day11/b/calendar/Main_calendar.java
@@ -0,0 +1,17 @@
+package my.day11.b.calendar;
+
+import java.util.Date;
+
+public class Main_calendar {
+
+	public static void main(String[] args) {
+		//자바에서 날짜를 나타낼때는 Date, Calendar를 사용한다
+		
+		//==1. Date 클래스르 사용하여 현재시각 나타내기 ===
+		Date now = new Date(); //현재시각,  util로 써야 함 
+		System.out.println(">> 현재시각 1 => " + now);
+		
+
+	}
+
+}
