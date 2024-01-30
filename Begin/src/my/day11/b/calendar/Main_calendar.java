cef5a48fe7914ee84c45bbc3559cdef417b66c76
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-01-30 12:37:29 +0900

    Update Main_calendar.java

--- Begin/src/my/day11/b/calendar/Main_calendar.java
+++ Begin/src/my/day11/b/calendar/Main_calendar.java
@@ -1,5 +1,6 @@
 package my.day11.b.calendar;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main_calendar {
@@ -11,7 +12,32 @@ public class Main_calendar {
 		Date now = new Date(); //현재시각,  util로 써야 함 
 		System.out.println(">> 현재시각 1 => " + now);
 		
-
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //import 해야함
+		
+		
+		System.out.println(">> 현재시각 2 => " + sdformat.format(now)); 
+		/*
+	       <참고>
+	         날짜 타입 format         설명
+	        --------------------------------------------------
+	           %tF              날짜를 yyyy-mm-dd 형식으로 포맷해주는 것
+	         %tT              날짜의 시각을 HH:MM:SS 형식으로 포맷해주는 것  
+	           %tp              오전, 오후를 출력
+	         %tA              요일명 출력
+	         %tY              4자리 년도만 출력
+	           %tB              월의 이름(January, February ...)출력
+	         %tm              월의 01,02,03 ~~ 10,11,12 로 출력
+	         %td              일수를 1~31 로 출력
+	         %te              %td와 동일함
+	         %tk              시간을 0~23 으로 출력
+	           %tl              시간을 1~12 으로 출력
+	         %tM              분을 00~59 으로 출력
+	           %tS              초를 00~59 으로 출력
+	         %tZ              타임존을 출력(한국은 KST로 나옴).
+	      */
+		String  currentTime = String.format("%tF %tp %tT %tA %tZ", now, now, now, now, now); //"%t" time 시간 
+		System.out.println(">> 현재시각 3 => " + currentTime);
+		
 	}
 
 }
