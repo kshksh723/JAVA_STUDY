ab01dd12d8d9db3e395e67d4ca20365531135531
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-02-14 11:16:14 +0900

    2/14

--- Begin/src/my/day20/a/io/InputStream_main_03.java
+++ Begin/src/my/day20/a/io/InputStream_main_03.java
@@ -44,32 +44,13 @@ Data Sourceㅇ======>ㅇ 프로그램 ㅇ======>ㅇ Data Destination
               print(String str),
               write(int b)             
 */      
-public class InputStream_main_02 {
+public class InputStream_main_03 {
 
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println(">> 영문자르 입력하세요 [종료하려면 x를  입력하세요] <<");
-	
-		int input = 0;
-		while((input = System.in.read())!= -1) {	//abcd엔터
-			// -1 은 키보드로 Ctrl + C(강제종료)를 입력한 값이다.
-	         // 키보드로 Ctrl + C(강제종료)를 하면 while 을 빠져 나간다.
-	         // System.in.read() 는 1byte 씩 읽어온다.
-			System.out.println("\n === 키보드로 부터 데이터 입력받기 ==="); 
-	         System.out.println("화면에 출력 [println 을 사용한 input] : " + input);
-	         System.out.print("화면에 출력 [write 을 사용한 input] : ");
-	        System.out.write(input);
-	      //   System.out.flush();
-	        /*
-            flush 란 ? 출력버퍼에 임시로 보관되어 스트림으로 출력될 때까지 대기중인 데이터를 스트림으로 내보내는 것을 flush 라고 한다.
-            write() 메소드는 flush() 메소드와 함께 사용되어야만 출력버퍼에 들어가 있던 내용이 모니터(또는 파일)에 출력된다.
-            그런데  flush() 메소드를 사용하지 않아도 모니터(또는 파일)에 출력되는 경우이라면 auto flush 가 적용되어졌기 때문이다. 
-         */
-	        
-	        System.out.println("");
-	        System.out.println("화면에 출력 [Println을 사용한 (char)input] : " +(char)input);
-		} // end of while
-		System.out.println("\n >>프로그램 종료 <<");
+	while() {
+		
+	}// end of while
 	
 	}// end of main
 
