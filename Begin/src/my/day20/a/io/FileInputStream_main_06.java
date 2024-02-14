e0c0fcc46667d62e0c2c17bdca66a9285eeb799f
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-02-14 12:48:23 +0900

    2/14

--- Begin/src/my/day20/a/io/FileInputStream_main_06.java
+++ Begin/src/my/day20/a/io/FileInputStream_main_06.java
@@ -6,38 +6,38 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /*
-		※ Data Source (File, 키보드, 원격 컴퓨터)
-		: 데이터의 근원
-		
-		※ Data Destination (파일, 모니터, 프린터, 메모리)
-		: 데이터가 최종적으로 도착하는 곳
-		
-		Data Sourceㅇ======>ㅇ 프로그램 ㅇ======>ㅇ Data Destination
-		             입력스트림          출력스트림
-		           InputStream       OutputStream          
-		
-		  
-		  === C:\NCS\iotestdata\korea.txt 파일을 읽어서 그 내용을 모니터(콘솔화면)에 출력하는 예제 ===
-		              
-		  1. 데이터소스   : 파일로 부터 읽어들임 (노드스트림: FileInputStream) 
-		  2. 데이터목적지 : 결과물을 모니터에 출력(노드스트림: System.out)
-		  
-		  >>>> FileInputStream
-		     - Node 스트림(접속점이 파일인 입력스트림)
-		     - 1byte 기반 스트림.
-		    
-		  >>>>> System.out :
-		        부모클래스가 추상클래스 OutputStream(기본 출력 스트림) 타입인 것으로서
-		        접속점(빨대)이 콘솔화면(모니터)인 출력 스트림(PrintStream)이다.
-		                
-		   -- Node(접속점)가 콘솔화면(모니터)인 출력스트림이다.
-		   -- 1byte 기반 스트림이다.
-		   -- 주요 메소드 : println(String str),
-		                 print(String str),
-		                 write(int b)      
-		           
-		*/
-public class FileInputStream_main_05 {
+※ Data Source (File, 키보드, 원격 컴퓨터)
+: 데이터의 근원
+
+※ Data Destination (파일, 모니터, 프린터, 메모리)
+: 데이터가 최종적으로 도착하는 곳
+
+Data Sourceㅇ======>ㅇ 프로그램 ㅇ======>ㅇ Data Destination
+             입력스트림          출력스트림
+           InputStream       OutputStream          
+
+  
+  === C:\NCS\iotestdata\korea.txt 파일을 읽어서 그 내용을 모니터(콘솔화면)에 출력하는 예제 ===
+              
+  1. 데이터소스   : 파일로 부터 읽어들임 (노드스트림: FileInputStream) 
+  2. 데이터목적지 : 결과물을 모니터에 출력(노드스트림: System.out)
+  
+  >>>> FileInputStream
+     - Node 스트림(접속점이 파일인 입력스트림)
+     - 1byte 기반 스트림.
+    
+  >>>>> System.out :
+        부모클래스가 추상클래스 OutputStream(기본 출력 스트림) 타입인 것으로서
+        접속점(빨대)이 콘솔화면(모니터)인 출력 스트림(PrintStream)이다.
+                
+   -- Node(접속점)가 콘솔화면(모니터)인 출력스트림이다.
+   -- 1byte 기반 스트림이다.
+   -- 주요 메소드 : println(String str),
+                 print(String str),
+                 write(int b)      
+           
+*/
+public class FileInputStream_main_06 {
 
 	public static void main(String[] args) {
 	
