ae155b261dbb5b5133d2dfc3da3b300fba16dcaf
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-03-17 05:11:00 +0900

    gitignore metadata

--- Mini_Project/src/member/controller/Member_Controller.java
+++ Mini_Project/src/member/controller/Member_Controller.java
@@ -179,6 +179,7 @@ do {
       default:
    	   
       	System.out.println(">> 메뉴에 없는 번호입니다. 다시 선택하세요 !! <<");
+      	System.out.println(">> 메뉴에 없는 번호입니다. 다시 선택하세요 !! <<");
       	break; 
       
      } //END OF  switch (s_choice) 
