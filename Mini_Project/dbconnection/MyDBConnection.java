219de50093c51a4769899ac9054a60faed953959
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-03-22 10:43:28 +0900

    1

--- Mini_Project/dbconnection/MyDBConnection.java
+++ Mini_Project/dbconnection/MyDBConnection.java
@@ -30,7 +30,7 @@ public class MyDBConnection {
 			try {
 				Class.forName("oracle.jdbc.OracleDriver");
 				
-				conn = DriverManager.getConnection("jdbc:oracle:thin:@211.238.142.195:1521:xe", "MINI_ORAUSER4", "gclass");
+				conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "MINI_ORAUSER4", "gclass");
 			} catch(ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch(SQLException e){
